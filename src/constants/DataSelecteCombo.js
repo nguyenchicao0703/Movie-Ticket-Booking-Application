74d380a0a019e95b00c01c1ddd70114d859e4a7f
Dungle-23 <97lehoangdung@gmail.com>
2023-11-02 21:02:14 +0700

    11.2

---
+++ src/constants/DataSelecteCombo.js
@@ -0,0 +1,41 @@
+const { default: ComboImage } = require('./ComboImage');
+
+const SelectCombo = [
+    {
+        id: 1,
+        name: 'BABY SHARK SINGLE COMBO - 195.000 đ',
+        decreption:
+            '1 ly Baby Shark + 1 nước siêu lớn. Mua thêm một bắp ngọt lớn chỉ 29k.  Nhận trong ngày xem phim.Mẫu ly phụ thuộc vào số lượng từng rạp  ',
+        image: ComboImage[0].image,
+    },
+    {
+        id: 2,
+        name: 'BABY SHARK SINGLE COMBO - 195.000 đ',
+        decreption:
+            '1 ly Baby Shark + 1 nước siêu lớn. Mua thêm một bắp ngọt lớn chỉ 29k.  Nhận trong ngày xem phim.Mẫu ly phụ thuộc vào số lượng từng rạp  ',
+        image: ComboImage[0].image,
+    },
+    {
+        id: 3,
+        name: 'BABY SHARK SINGLE COMBO - 195.000 đ',
+        decreption:
+            '1 ly Baby Shark + 1 nước siêu lớn. Mua thêm một bắp ngọt lớn chỉ 29k.  Nhận trong ngày xem phim.Mẫu ly phụ thuộc vào số lượng từng rạp  ',
+        image: ComboImage[0].image,
+    },
+    {
+        id: 4,
+        name: 'BABY SHARK SINGLE COMBO - 195.000 đ',
+        decreption:
+            '1 ly Baby Shark + 1 nước siêu lớn. Mua thêm một bắp ngọt lớn chỉ 29k.  Nhận trong ngày xem phim.Mẫu ly phụ thuộc vào số lượng từng rạp  ',
+        image: ComboImage[0].image,
+    },
+    {
+        id: 5,
+        name: 'BABY SHARK SINGLE COMBO - 195.000 đ',
+        decreption:
+            '1 ly Baby Shark + 1 nước siêu lớn. Mua thêm một bắp ngọt lớn chỉ 29k.  Nhận trong ngày xem phim.Mẫu ly phụ thuộc vào số lượng từng rạp  ',
+        image: ComboImage[0].image,
+    },
+];
+
+export { SelectCombo };
