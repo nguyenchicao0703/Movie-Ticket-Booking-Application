9221912ab76360a773dd7827af3f91842a67037e
Dungle-23 <97lehoangdung@gmail.com>
2023-11-10 22:30:24 +0700

    1
    Cao m test dùm t xem thử api get đc kh tự nhiên máy t bị nework error

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -2,6 +2,8 @@ import axios from 'axios';
 
 const BASE_URL = 'http://10.0.2.2:1234/api'; // URL cơ sở của API
 
+// const BASE_URL = 'http://127.0.0.1:1234/api'; // URL cơ sở của API máy thật
+
 // Hàm chung để gửi yêu cầu HTTP
 const axiosClient = (method, url, data) => {
     return axios
