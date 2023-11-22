bd86a0397634c40b467caa44bca9d43d1b75b8e0
nguyenchicao0703 <chicao0703@gmail.com>
2023-11-22 16:10:21 +0700

    update api

--- src/api/showtimesAPI.js
+++ src/api/showtimesAPI.js
@@ -4,13 +4,13 @@ import axiosClient from './axiosClient';
 const showtimesAPI = {
     getAllMovies: (idMovie, dateMovie) => {
         return axiosClient.get(
-            `/Danh-sach-suat-chieu-phim.php?id_phim=${idMovie}&ngay=2023-12-30`,
+            `/Danh-sach-suat-chieu-phim.php?id_phim=${idMovie}&ngay=${dateMovie}`,
         );
     },
     getAllCinemas: (idCinema, dateMovie) => {
         console.log({ idCinema }, { dateMovie });
         return axiosClient.get(
-            `/Danh-sach-suat-chieu-rap.php?id_rap=${idCinema}&ngay=2023-12-30`,
+            `/Danh-sach-suat-chieu-rap.php?id_rap=${idCinema}&ngay=${dateMovie}`,
         );
     },
 };
